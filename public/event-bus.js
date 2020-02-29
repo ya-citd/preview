a026fbb8a2db2ee4f4a3de2936ebe3b592973634
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-02-29 14:48:53 +0300

    переключение между окнами

--- public/event-bus.js
+++ public/event-bus.js
@@ -1,8 +1,8 @@
 const EVENTS = {};
 
 window.addEventListener('storage', function(e) {
-    if (e.storageArea === localStorage && e.key.startsWith('event-')) {
-        trigger1(e.key, e.newValue);
+    if (e.storageArea === localStorage && e.key.startsWith('event-') && e.newValue !== null) {
+        handle1(e.key, e.newValue);
     }
 });
 
@@ -11,8 +11,13 @@ function bind1(name, fn) {
     EVENTS[name].push(fn); 
 }
 
-function trigger1(name, data) {
+function handle1(name, data) {
     (EVENTS[name] || []).forEach(fn => {
         try { fn(data); } catch (e) { console.error(e); }
     });
+}
+
+function trigger1(name, data) {
+    localStorage.removeItem(name);
+    localStorage.setItem(name, data);
 }
